6cabe7deced659d29e4f9bc7d3ed81e8a18812a4
tarek mourad <mouradtarek08@gmail.com>
2025-04-15 09:05:47 +0300

    IUserDbContext

--- DataAccess/User/IUserDbContext.cs
+++ DataAccess/User/IUserDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,15 @@ namespace DataAccess.User
 {
     public class IUserDbContext: IdentityDbContext<User>
     {
+        public IUserDbContext(DbContextOptions<IUserDbContext> options)
+            : base(options)
+        {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
