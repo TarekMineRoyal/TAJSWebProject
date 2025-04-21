65933128c0887062cab753c1c830650a676e6b7e
JafarMahmood123 <gafarARC.com@gmail.com>
2025-04-21 22:27:10 +0300

    Add the functionalities to the CarService to be implemented

--- BLL/IServices/CarService.cs
+++ BLL/IServices/CarService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataAccess.Entities;
+using DataAccess.Repository.IRepository;
+using DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,37 @@ namespace BLL.IServices
 {
     public class CarService
     {
+        private readonly IGenericRepository<Car> _genericRepository;
 
+        public CarService(IGenericRepository<Car> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+
+        public Task<CarDTO?> GetCarByIdAsync(int id)
+        {
+            return null;
+        }
+
+        public Task<IEnumerable<CarDTO>?> GetAllCarsAsync()
+        {
+            return null;
+        }
+
+        public Task<CreateCarDTO> AddCarAsync(CreateCarDTO car)
+        {
+            return null;
+        }
+
+        public Task<UpdateCarDTO> UpdateCarAsync(int id, UpdateCarDTO car)
+        {
+            return null;
+        }
+
+        public Task<CarDTO> DeleteCar(int id)
+        {
+            return null;
+        }
     }
 }
