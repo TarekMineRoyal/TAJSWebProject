6ee865dde5a0968ad28f9517ad73c2dbd2340095
AbSamrah <syria.aa.2004@gmail.com>
2025-04-21 22:15:03 +0300

    adding 2 services

---
+++ BLL/IServices/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.IServices
+{
+    public interface IPaymentService
+    {
+        Task Pay(User user, double amount);
+    }
+}
