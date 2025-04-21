dc99a2c6051149298b782fac044b48a370473f0d
JafarMahmood123 <gafarARC.com@gmail.com>
2025-04-21 22:08:01 +0300

    Adding the GetCarById and GetAllCars APIs

---
+++ API/Controllers/CarsController.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Entities;
+using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class CarsController : Controller
+{
+    private readonly IGenericRepository<Car> _genericRepository;
+
+    public CarsController(IGenericRepository<Car> genericRepository)
+    {
+        _genericRepository = genericRepository;
+    }
+
+    public async Task<IActionResult> GetCarById(int id)
+    {
+        var car = await _genericRepository.GetByIdAsync(id);
+
+        if(car == null) 
+            return NotFound();
+
+        return Ok(car);
+    }
+
+    public async Task<IActionResult> GetAllCars()
+    {
+        return Ok(await _genericRepository.GetAllAsync());
+    }
+
+
+}
