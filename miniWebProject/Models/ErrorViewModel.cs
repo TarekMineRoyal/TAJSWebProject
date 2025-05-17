4d2893b205f47ca094820c608ac05e163bade892
Tarek Mourad <mouradtarek08@gmail.com>
2025-05-17 12:30:51 +0300

    Delete miniWebProject directory

--- miniWebProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace miniWebProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
