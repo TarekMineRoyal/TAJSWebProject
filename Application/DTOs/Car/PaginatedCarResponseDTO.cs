0beaf8cdcb0e227b051043db74dccdd31ee97c0d
JafarMahmood123 <gafarARC.com@gmail.com>
2025-06-18 03:15:05 +0300

    Add the car repository with sorting,filtering and pagenation on getAllCars and GetAllAvialableCars

---
+++ Application/DTOs/Car/PaginatedCarResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿// Application/DTOs/Car/CarQueryModels.cs
+
+using System.Collections.Generic;
+
+namespace Application.DTOs.Car
+{
+    public class PaginatedCarResponseDTO
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+        public IEnumerable<CarDTO> Cars { get; set; }
+    }
+}
---
+++ Application/IRepositories/ICarRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.Car;
+using Domain.Entities.AppEntities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.IRepositories
+{
+    /// <summary>
+    /// Defines a repository specifically for Car entities, extending the generic repository.
+    /// </summary>
+    public interface ICarRepository : IGenericRepository<Car>
+    {
+        /// <summary>
+        /// Gets a paginated, filtered, and sorted list of cars.
+        /// </summary>
+        /// <param name="queryParameters">The parameters for filtering, sorting, and pagination.</param>
+        /// <param name="excludeCarIds">An optional list of car IDs to exclude from the result.</param>
+        /// <returns>A tuple containing the list of cars for the current page and the total count of cars matching the filter.</returns>
+        Task<(IEnumerable<Car> Cars, int TotalCount)> GetFilteredCars(CarQueryParameters queryParameters, IEnumerable<int> excludeCarIds = null);
+    }
+}
